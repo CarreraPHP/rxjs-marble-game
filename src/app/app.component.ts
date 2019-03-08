3f3e4d2a5453bc08f2247aaf749057e0e1b0958a
Yogesh Surendran <37717@hexaware.com>
2019-03-08 20:26:27 +0530

    add window resize logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { AppService, ScreenSize } from './app.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent {
-  title = 'rxjs-marble-game';
+	title = 'rxjs-marble-game';
+
+	@HostListener('window:resize', [ '$event' ])
+	onWinResize(event?) {
+		const { innerWidth, innerHeight } = window;
+		this.appService.scrnMetrics = <ScreenSize>{ h: innerHeight, w: innerWidth };
+	}
+
+	constructor(private appService: AppService) {
+		this.onWinResize();
+	}
 }
--- step-by-step.md
+++ step-by-step.md
@@ -3,3 +3,4 @@ ng add @angular/material (Provides question to add "Hammerjs" & "@angular/animat
 ng g m game --module app --routing --routing-scope --flat false
 ng g c game/canvas --flat false --module game --prefix rmg --export --change-detection OnPush
 ng g c game/grid --flat false --module game --prefix rmg --export --change-detection OnPush
+ng g s app

149ac1100b1df731f48981e67ac7d2325e65cd56
CarreraPHP <carreraphp@gmail.com>
2019-03-10 23:56:55 +0530

    service to handle the game canvas

--- src/app/game/canvas/canvas.component.ts
+++ src/app/game/canvas/canvas.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { AppService, ScreenSize } from "../../app.service";
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'rmg-canvas',
@@ -7,8 +9,20 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CanvasComponent implements OnInit {
+  contentSize:ScreenSize;
 
-  constructor() { }
+  constructor(private appService: AppService, private cdRef:ChangeDetectorRef) {
+    this.handleContentSize();
+  }
+
+  handleContentSize() {
+    const contentMetrics$: Observable<ScreenSize> = this.appService.availableContentMetricsObservable;
+    contentMetrics$.subscribe((size: ScreenSize) => {
+      console.log("Height x width", size);
+      this.contentSize = {...size};
+      this.cdRef.markForCheck();
+    });
+  }
 
   ngOnInit() {
   }
---
+++ src/app/game/canvas/canvas.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { AppService } from 'src/app/app.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CanvasService {
+  rem: number;
+  grid: string[][] = [];
+
+  get cellCnt(): number {
+    return this.appService.cellCnt;
+  }
+
+	get visibleCellCnt(): number {
+		return this.cellCnt - 1;
+  }
+
+	get colCnt(): number {
+		return this.appService.colCnt;
+	}
+	
+  constructor(private appService:AppService) {}
+
+  /**
+   * populate the grid by the count provided.
+   */
+	populateGrid(): string[][] {
+		let ret: string[][] = [];
+		for (let i = 0; i < this.colCnt; i++) {
+			for (let j = 0, col: string[] = []; j < this.cellCnt; j++) {
+				col = [ ...col, ...[ `(${i + 1},${j + 1})` ] ];
+				// col = [ ...col, ...[ `${this.hues[i * j]}` ] ];
+				if (j == this.cellCnt - 1) {
+					ret = [ ...ret, ...[ col ] ];
+				}
+			}
+		}
+		return this.grid = ret;
+  }
+  
+	handleRemChange() {
+		this.appService.fontSizeObservable.subscribe((v: number) => {
+			this.rem = v;
+		});
+	}
+}
